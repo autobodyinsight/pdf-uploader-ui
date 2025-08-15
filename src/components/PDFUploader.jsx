e8420835becf97dd16ff7187b4e9fa26ba5dad46
autobodyinsight <jorge@autobodyinsight.com>
2025-08-14 23:05:45 -0700

    Create PDFUploader.jsx

---
+++ src/components/PDFUploader.jsx
@@ -0,0 +1,13 @@
+function PDFUpload() {
+  return (
+    <div>
+      <input
+        type="file"
+        accept="application/pdf"
+        className="border border-orange-500 p-2 rounded"
+      />
+    </div>
+  );
+}
+
+export default PDFUpload;
