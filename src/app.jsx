927ceb9ccc38f82d0013775af08835c78f3bf2fc
autobodyinsight <jorge@autobodyinsight.com>
2025-08-14 23:05:09 -0700

    Create app.jsx

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import PDFUpload from './components/PDFUpload';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-black text-white p-4">
+      <h1 className="text-orange-500 text-2xl mb-4">PDF Uploader</h1>
+      <PDFUpload />
+    </div>
+  );
+}
+
+export default App;
